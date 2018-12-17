e5615936e60dc84b5dad5133720a745db47e9664
Tim-Hinnerk Heuer <heuert@landcareresearch.co.nz>
2018-12-17 22:56:14 +1300

    Initial import.

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+const json = {
+    id: 123,
+    numberCol: 1.23,
+    stringCol: "Hello world",
+    booleanCol: true,
+    arrayCol: [
+        "hello",
+        "world"
+    ]
+}
+const json2 = {
+    id: 234,
+    numberCol: 2.34,
+    stringCol: "Hello two",
+    booleanCol: false,
+    arrayCol: [
+        "you",
+        "two"
+    ]
+}
+
+const container = {
+    someStuff: [
+        json,
+        json2
+    ]
+}
